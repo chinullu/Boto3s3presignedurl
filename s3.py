975441a2e2581d32c74f2f062f7a19b3077316f9
Nithin Kumar Kandagatla <nithinkandagatla@gmail.com>
2017-01-20 14:42:42 -0500

    Create s3.py

---
+++ s3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+ 
+import boto3
+ 
+BUCKET = 'YOUR_BUCKET_NAME'
+KEY = 'YOUR_KEY'
+ 
+s3 = boto3.client('s3')
+print s3.generate_presigned_url(
+    ClientMethod='put_object',
+    Params={'Bucket': BUCKET, 'Key': KEY},
+    ExpiresIn=86400,
+    HttpMethod='PUT')
+   
