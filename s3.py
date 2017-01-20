ac07084cf73f43794ccba7a5cfa0a48492e2c2b3
Nithin Kumar Kandagatla <nithinkandagatla@gmail.com>
2017-01-20 14:57:15 -0500

    Update s3.py

--- s3.py
+++ s3.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
- 
+
+#Import 
 import boto3
- 
-BUCKET = 'YOUR_BUCKET_NAME'
-KEY = 'YOUR_KEY'
+#Validate Bucket name and keyphrases before running this program  
+BUCKET = 'MyBucket'
+KEY = 'KeyPhrases'
  
 s3 = boto3.client('s3')
 print s3.generate_presigned_url(
     ClientMethod='put_object',
     Params={'Bucket': BUCKET, 'Key': KEY},
-    ExpiresIn=86400,
+    ExpiresIn=86400,#Here 86400 seconds means 60*60*24(1day) The url expires in 1 day
     HttpMethod='PUT')
   
